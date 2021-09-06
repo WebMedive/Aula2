8d6d6ecc5df35a524ca103fc8416f74a04967036
WebMedive <and-nessa@hotmail.com>
2021-09-06 19:07:23 -0300

    Aula Node

---
+++ AulaNode/aula1.js
@@ -0,0 +1,10 @@
+nome = "Anderson de Carvalho"
+idade = 38
+cpf = 3456789012
+rg = 234567890
+endereco = "Rua Rafael"
+n = 234
+cidade = "Itapevi"
+cep = 98765743
+
+console.log("O participante " + nome + " realizou o cadastro e passou seus dados pessoais que são: Idade: " + idade + ", CPF: " + cpf + ", RG: " + rg + ", residente na rua: " + endereco + ", número " + n + ", cidade: " + cidade + ", cep: " + cep)
